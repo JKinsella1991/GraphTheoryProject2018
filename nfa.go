e0dc1c7a7c5b92f4a0dc49dcb3646cb30476667b
James Kinsella <G00282261@gmit.ie>
2018-04-08 23:18:41 +0100

    nfa - More cases

--- nfa.go
+++ nfa.go
@@ -42,6 +42,36 @@ func Poregtonfa(pofix string) *nfa {
 			frag2.accept.edge1 = &accept
 
 			nfaStack = append(nfaStack, &nfa{initial: &initial, accept: &accept})
+
+		case '*':
+			frag := nfaStack[len(nfaStack)-1]
+			nfaStack = nfaStack[:len(nfaStack)-1]
+
+			accept := state{}
+			initial := state{edge1: frag.initial, edge2: &accept}
+			frag.accept.edge1 = frag.initial
+			frag.accept.edge2 = &accept
+
+			nfaStack = append(nfaStack, &nfa{initial: &initial, accept: &accept})
+
+		case '?':
+			frag := nfaStack[len(nfaStack)-1]
+			nfaStack = nfaStack[:len(nfaStack)-1]
+
+			initial := state{edge1: frag.initial, edge2: frag.accept}
+
+			nfaStack = append(nfaStack, &nfa{initial: &initial, accept: frag.accept})
+
+		case '+':
+			frag := nfaStack[len(nfaStack)-1]
+			nfaStack = nfaStack[:len(nfaStack)-1]
+
+			accept := state{}
+			initial := state{edge1: frag.initial, edge2: &accept}
+
+			frag.accept.edge1 = &initial
+
+			nfaStack = append(nfaStack, &nfa{initial: frag.initial, accept: &accept})
 	}
 	return ismatch
 }
