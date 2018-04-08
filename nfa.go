71c5ba416da809b23a9bd7638959b5b39b933937
James Kinsella <G00282261@gmit.ie>
2018-04-08 23:16:33 +0100

    nfa - cases

--- nfa.go
+++ nfa.go
@@ -16,8 +16,32 @@ type nfa struct {
 
 func Poregtonfa(pofix string) *nfa {
 	nfaStack := []*nfa{}
+
+	for _, r := range pofix {
+		switch r {
 		case '.':
-			
+		case '.':
+			frag2 := nfaStack[len(nfaStack)-1]
+			nfaStack = nfaStack[:len(nfaStack)-1]
+			frag1 := nfaStack[len(nfaStack)-1]
+			nfaStack = nfaStack[:len(nfaStack)-1]
+
+			frag1.accept.edge1 = frag2.initial
+
+			nfaStack = append(nfaStack, &nfa{initial: frag1.initial, accept: frag2.accept})
+
+		case '|':
+			frag2 := nfaStack[len(nfaStack)-1]
+			nfaStack = nfaStack[:len(nfaStack)-1]
+			frag1 := nfaStack[len(nfaStack)-1]
+			nfaStack = nfaStack[:len(nfaStack)-1]
+
+			accept := state{}
+			initial := state{edge1: frag1.initial, edge2: frag2.accept}
+			frag1.accept.edge1 = &accept
+			frag2.accept.edge1 = &accept
+
+			nfaStack = append(nfaStack, &nfa{initial: &initial, accept: &accept})
 	}
 	return ismatch
 }
