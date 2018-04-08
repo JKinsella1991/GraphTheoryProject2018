fc573c4f3209787f82b9d5b52d69bcd3d5fae4c2
James Kinsella <G00282261@gmit.ie>
2018-04-08 22:36:14 +0100

    Restructured Shunt

--- shunt.go
+++ shunt.go
@@ -1,35 +1,49 @@
 //James Kinsella - G00282261@gmit.ie
 //Graph Theory Project 2018
 
-package main
+package Graph_Project
 
-import {
-	"fmt"
+
+
+func Intopost(infix string) string {
+	specials := map[rune]int{'*': 10, '.': 9, '|': 8}
+	pofix := []rune{}
+	s := []rune{}
+
+	for _, r := range infix {
+		switch {
+		case r == '(':
+			s = append(s, r)
+		case r == ')':
+			for s[len(s)-1] != '(' {
+				pofix = append(pofix, s[len(s)-1])
+				s = s[:len(s)-1]
+			}
+			s = s[:len(s)-1]
+		case specials[r] > 0:
+			for len(s) > 0 && specials[r] <= specials[s[len(s)-1]] {
+
+				pofix = append(pofix, s[len(s)-1])
+				s = s[:len(s)-1]
+			}
+
+			s = append(s, r)
+
+		default:
+			pofix = append(pofix, r)
+		}
+	}
+	for len(s) > 0 {
+		pofix = append(pofix, s[len(s)-1])
+		s = s[:len(s)-1]
+	}
+	return string(pofix)
 }
 
-func intopost(infix string) string {
-	postfix := ""
+func StringTrim(s string) string {
 
-	return postfix
-}
-
-func main() {
-	// Answer ab.c*.
-	fmt.PrintLn("Infix:	","a.b.c*")
-	fmt.PrintLn("Postfix: ", intopost("a.b.c*"))
-
-	// Answer abd|.*
-	fmt.PrintLn("Infix:	","(a.(b|d))*")
-	fmt.PrintLn("Postfix: ", intopost("(a.(b|d))*"))
-
-	// Answer abd|.c*
-	fmt.PrintLn("Infix:	","a.(b|d).c*")
-	fmt.PrintLn("Postfix: ", intopost("a.(b|d).c*"))
-
-	// Answer abb.+.c.
-	fmt.PrintLn("Infix:	","a.(b.b)+.c*")
-	fmt.PrintLn("Postfix: ", intopost("a.(b.b)+.c*"))
-
-
-
+	if len(s) > 0 {
+		s = s[:len(s) - 2]
+	}
+	return s
 }
